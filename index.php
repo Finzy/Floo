67f55737c8205aa723a8a11b0532ff75b9c9c8e0
RicoSnoek <rico-8888@hotmail.com>
2015-09-02 13:52:07 +0200

    portfolio add

---
+++ index.php
@@ -0,0 +1,10 @@
+    <?php include 'php/header.php' ?>
+<section id="container">
+    <?php include 'php/home.php' ?>
+</section>
+
+<!--moeten deze dingen niet in de container?-->
+    <?php include 'php/overOns.php' ?>
+    <?php include 'php/portfolio.php' ?>
+    <?php include 'php/contact.php' ?>
+    <?php include 'php/footer.php' ?>
