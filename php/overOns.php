fc6e552f862b6f2bd67010819d8b69b884341645
Finzy <perryzetten@gmail.com>
2015-09-02 13:57:02 +0200

    last changes

--- php/overOns.php
+++ php/overOns.php
@@ -3,45 +3,37 @@
     <div class="floating about_area perry">
     	<img src="http://devimg.com/x">
     	<h1>Perry van Zetten</h1>
-    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    	<p>Hallo, ik ben Perry van Zetten ik kom uit Amsterdam, Noord-Holland en ben 18 jaar oud, ik werk nu
+        3,5 jaar in een restaurant in het centrum van Amsterdam. Ik hou me vooral bezig
+        met front & back-end development waarvan ik front-end het leukste vind om te doen.
+        In me vrije tijd ben ik vooral bezig met brommers wat iets totaal anders is dan
+        waar ik met op school mee bezig hou maar ik vind het leuk om meerdere dingen te leren.
+        </p>
     </div>
 
         <div class="floating2 about_area rico">
     	<img src="http://devimg.com/x">
-    	<h1>Perry van Zetten</h1>
-    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    	<h1>Mark Jongbloed</h1>
+    	<p>Hey! Ik ben Mark Jongbloed ik ben 19 jaar oud en kom uit Heemskerk, Noord-Holland.
+        Momenteel werk ik bij de Jumbo als vakkenvuller. Het leukste om te maken vind ik 
+        visitekaartjes, posters, logo's en flyers. Dit is ook een van mijn vele bezigheden
+        op school. In mijn vrijetijd maak ik graag muziek, dat is ook een van mijn grote hobbies.</p>
     </div>
 
     <div class="floating3 about_area mark">
     	<img src="http://devimg.com/x">
-    	<h1>Perry van Zetten</h1>
-    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    	<h1>Noeel Moeskop</h1>
+    	<p>Hallo, mijn naam is Noeel Moeskop ik ben 19 jaar oud en woon in Hoorn, Noord-Holland.
+        Op school vind ik het leuk om te designen, het maakt mij niet uit wat ik design ik vind
+        alles wel leuk. Ook doe ik graag aan front-end development.</p>
     </div>
 
     <div class="floating about_area noeel">
     	<img src="http://devimg.com/x">
-    	<h1>Perry van Zetten</h1>
-    	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-    	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-    	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-    	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-    	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    	<h1>Rico Snoek</h1>
+    	<p>Hoi, ik ben Rico Snoek, woon in Volendam, Noord-Holland en ben 18 jaar oud.
+        Een van mijn bezigheden op school is back-end development en in mijn vrije tijd
+        vind ik het leuk om te gamen.</p>
     </div>
 
 </section> 
