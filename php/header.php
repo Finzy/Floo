03b451a1a94917157ce6bf24281b697769635f74
Perry <perryzetten@gmail.com>
2015-09-07 19:55:33 +0200

    noeels code

--- php/header.php
+++ php/header.php
@@ -7,8 +7,22 @@
     <link rel="icon" href="media/img/favicon.gif" type="image/x-icon" />
     <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
     <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-    <script src="js/portfolio.js"></script>
+<!--    <script src="js/portfolio.js"></script>-->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    
+<!--    <link rel="stylesheet" href="stylesheets/css/bolletjesSliker.css">-->
+
+    
+<!--    page transision js-->
+<!--
+<link rel="stylesheet" href="stylesheets/css/animations.css">
+<link rel="stylesheet" href="stylesheets/css/component.css">
+<link rel="stylesheet" href="stylesheets/css/default.css">
+<link rel="stylesheet" href="stylesheets/css/multilevelmenu.css">
+<script src="js/jquery.dlmenu.js"></script>
+<script src="js/modernizr.custom.js"></script>
+<script src="js/pagetransitions.js"></script>
+-->
 
 </head>
 
@@ -18,12 +32,12 @@
         <div id="achtergrondBol"></div>       
     </div>
     <header>
-        <a class="nav_contact" href="?u=contact">Contact</a>
+        <a to="contact" class="nav_contact" >Contact</a>
 <!--        <a class="nav_portfolio" href="?u=portfolio">Portfolio</a>-->
-        <a class="nav_about" href="?u=about">Wie zijn wij</a>
+        <a to="overOns" class="nav_about" >Wie zijn wij</a>
         
         <div>
-           <a href="?u=index">
+           <a to="index">
             <img src="media/img/floooWeb300px.gif" alt="flooo">
             </a>
         </div>
--- php/home.php
+++ php/home.php
@@ -1,16 +1,74 @@
-<link rel="stylesheet" href="stylesheets/css/bolletjesSliker.css">
+<div class="bolContainer">
+    <div id="bolletjesSlikker">
+        <div class="bol"></div>
+        <div class="bol"></div>
+        <div class="bol"></div>
+        <div class="bol"></div>
+    </div>
+</div>
+
+
 <img id="logoBig" src="media/img/floooWeb1200px.gif" alt="flooo">
 
 
 <div class="keywords">
-	<h2>Websites</h2>
-	<h2>Wordpress thema's</h2>
-	<h2>Posters</h2>
-	<h2>Visitekaartjes</h2>
-	<h2>Web-applicaties</h2>
-	<h2>logo's</h2>
-	<h2>Advertenties</h2>
-	<h2>Apps</h2>	
-	<h2>Apps</h2>	
-	<h2>Apps</h2>	
+   <span>
+        <h2>Animaties</h2>
+        <p>Net zoals onze website kunnen wij uw website interactief maken. Dit is ook onze specialiteid. En daar zijn we ook erg goed in
+</p>
+    </span>
+    <span>
+        <h2>Websites</h2>
+        <p>Deze website is volledig door ons gemaakt. Dit  zouden wij ook graag voor u doen.
+</p>
+    </span>
+    <span>
+        <h2>Wordpress (thema's)</h2>
+        <p>We zijn ook erg handig met wordpress. We kunnen thema's maken, wordspress servers en nog veel meer. 
+</p>
+    </span>
+    <span>
+        <h2>Posters</h2>
+        <p>Bekijk onze poster! Dit zouden wij ook voor u kunnen maken.
+</p>
+    </span>
+    <span>
+        <h2>Visitekaartjes</h2>
+        <p>We maken een ontwerp naar wens voor u!
+</p>
+    </span>
+    <span>
+        <h2>Web-applicaties</h2>
+        <p>Web-applicaties (bijv. Facebook apps) worden gerealiseerd
+</p>
+    </span>
+    <span>
+         <h2>logo's</h2>
+        <p>compleet logo design met meerdere keuzen
+</p>
+    </span>
+    <span>
+        <h2>Advertenties</h2>
+        <p>een design voor uw advertentie en/of een reclame campagne verzinnen.
+</p>
+    </span>
+    <span>
+        <h2>Apps</h2>
+        <p>Apps voor uw telefoon kunnen gerealiseerd worden</p>
+    </span>
+    <span>
+        <h2>Flyers</h2>
+      <p>Promotieflyers om uw publiek te bereiken
+</p>
+   </span>
+       <span>
+        <h2>User Experience Design</h2>
+      <p>Wij besteden ook veel aandacht aan udx dit houd in de ervaring die de gebruiker met het product heeft.
+</p>
+   </span>
+    <span>
+        <h2>Tijdschriften</h2>
+        <p>Wij kunnen ook uw design van een tijdschrift maken.
+</p>
+    </span>
 </div>
